88c771125c5749dfc2d20dd3a98d492d8fb2aea2
hp <hp@DK>
2016-05-23 07:29:33 +0100

    Add project files.

---
+++ ConsultationManager/Views/Home.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ConsultationManager.Views
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+        public List<EducationContent> EducationContents { get { return ConsultationManager.Views.EducationContents.DataSource; } }
+        public List<EmployeeContent> EmployeeContents { get { return ConsultationManager.Views.EmployeeContents.DataSource; } }
+    }
+
+    public class EducationContent
+    {
+        public string HeaderCont { get; set; }
+        public string TextCont { get; set; }
+        public string Photo { get; set; }
+        public ImageSource PhotoSource
+        {
+            get
+            {
+                return string.IsNullOrEmpty(Photo) ? null : new BitmapImage(new Uri(Photo, UriKind.Relative));
+            }
+        }
+    }
+    public static class EducationContents
+    {
+        public static readonly List<EducationContent> DataSource =
+            new List<EducationContent> {
+                new EducationContent { HeaderCont = "Education thérapeutique", TextCont = "Visualiser tous les rapport d'éducation thérapeutique", Photo = "/ConsultationManager;component/Imgs/Home/education.png" },
+                new EducationContent { HeaderCont = "Education thérapeutique", TextCont = "Editer un rapport d'éducation thérapeutique", Photo = "/ConsultationManager;component/Imgs/Home/education-rapport.png" }
+            };
+    }
+
+    public class EmployeeContent
+    {
+        public string HeaderCont { get; set; }
+        public string TextCont { get; set; }
+        public string Photo { get; set; }
+        public ImageSource PhotoSource
+        {
+            get
+            {
+                return string.IsNullOrEmpty(Photo) ? null : new BitmapImage(new Uri(Photo, UriKind.Relative));
+            }
+        }
+    }
+    public static class EmployeeContents
+    {
+        public static readonly List<EmployeeContent> DataSource =
+            new List<EmployeeContent> {
+                new EmployeeContent { HeaderCont = "Employées", TextCont = "Afficher et modifier la liste des employées", Photo = "/ConsultationManager;component/Imgs/Home/staff.jpg" },
+                new EmployeeContent { HeaderCont = "Accès", TextCont = "Controller les droits d'accès des employées", Photo = "/ConsultationManager;component/Imgs/Home/roles.png" }
+            };
+    }
+}
