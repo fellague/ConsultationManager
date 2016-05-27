6d7cb5b92adb8bd1f35da1f28e948c154163285d
hp <hp@DK>
2016-05-27 05:12:06 +0100

    attempt 1 failed to ad a dialog for new Rdv

--- ConsultationManager/Views/RDVs/AllMyRdvPage.xaml.cs
+++ ConsultationManager/Views/RDVs/AllMyRdvPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MaterialDesignColors;
+using MaterialDesignThemes;
 
 namespace ConsultationManager.Views.RDVs
 {
@@ -36,5 +38,7 @@ namespace ConsultationManager.Views.RDVs
             }
             listViewAllMyRdv.ItemsSource = myList;
         }
+
+        public ICommand RunDialogCommand => new AnotherCommandImplementation()
     }
 }
